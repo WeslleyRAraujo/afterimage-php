268aaf486221cf41efe34103c0ecb93f9ebbf1e9
WeslleyRAraujo <weslleygithub@gmail.com>
2022-02-14 14:31:35 -0300

    update index

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 include_once "./bootstrap.php";
 
-$route = new \Afterimage\Router();
+$route = new \Afterimage\Router;
 
 $route->get('/', 'App\Controller\HomeController:index');
 $route->any('/message', 'App\Controller\HomeController:json');
