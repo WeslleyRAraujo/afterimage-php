9b95d239cf4462814765308c53f3b95e070accb6
Weslley Araujo <weslleygithub@gmail.com>
2022-02-16 09:04:37 -0300

    update callback

--- public/index.php
+++ public/index.php
@@ -5,5 +5,5 @@ include_once "./bootstrap.php";
 $route = new \Afterimage\Router;
 
 $route->get('/', 'App\Controller\HomeController:index');
-$route->any('/message/:p', 'App\Controller\HomeController:json')->param("any");
+$route->any('/message/:p', 'App\Controller\HomeController:message')->param("any");
 
